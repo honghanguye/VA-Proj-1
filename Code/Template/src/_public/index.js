d7fc040e7ae6672f4a376be3e4cc731d48e663ec
honghanguye <92738940+honghanguye@users.noreply.github.com>
2024-05-17 14:42:24 +0200

    pcp done

--- Code/Template/src/_public/index.js
+++ Code/Template/src/_public/index.js
@@ -1,32 +1,43 @@
-import io from "socket.io-client"
-import "./app.css"
-import {configs} from "../_server/static/configs.js"
-import { drawPCP } from "./pcp.js"
-import * as d3 from "d3"
+import io from "socket.io-client";
+import "./app.css";
+import { configs } from "../_server/static/configs.js";
+import { drawPcp } from "./newpcp.js";
+import * as d3 from "d3";
 
-let hostname = window.location.hostname
-let protocol = window.location.protocol
-const socketUrl = protocol + "//" + hostname + ":" + configs.port
+let hostname = window.location.hostname;
+let protocol = window.location.protocol;
+const socketUrl = `${protocol}//${hostname}:${configs.port}`;
 
-export const socket = io(socketUrl)
+export const socket = io(socketUrl);
+
 socket.on("connect", () => {
-  console.log("Connected to " + socketUrl + ".")
-})
+  console.log("Connected to " + socketUrl + ".");
+});
+
 socket.on("disconnect", () => {
-  console.log("Disconnected from " + socketUrl + ".")
-})
+  console.log("Disconnected from " + socketUrl + ".");
+});
 
 /**
- * Callback, when the button is pressed to request the data from the server.
+ * Request data from the server based on parameters.
  * @param {*} parameters
  */
 let requestData = (parameters) => {
-  console.log(`requesting data from webserver (every 2sec)`)
+  console.log(`Requesting data from webserver with parameters:`, parameters);
+  socket.emit("getData", { parameters });
+};
 
-  socket.emit("getData", {
-    parameters,
-  })
-}
+/**
+ * Handle data received from the server.
+ * @param {*} payload
+ */
+let handleData = (payload) => {
+  console.log("Data received from server:", payload);
+  data.pcp = payload.data;
+  drawPcp(data.pcp);
+};
+
+socket.on("freshData", handleData);
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("load_data_button").onclick = () => {
@@ -42,99 +53,16 @@ document.addEventListener("DOMContentLoaded", function() {
     const mechanicElement = document.getElementById("mechanic");
     const mechanic = mechanicElement ? mechanicElement.value : null;
 
-    const parameters = {
-      top_rank, // Renamed variable
-      category,
-      mechanic
-    };
+    const parameters = { top_rank, category, mechanic };
 
-    // Call the function to request data based on the selected category
+    // Call the function to request data based on the selected parameters
     requestData(parameters);
   };
 });
 
-
-let requestDataByCategory = (parameters) => {
-  console.log(`requesting data from webserver (every 2sec)`);
-
-  socket.emit("getDataByCategory", {
-    parameters,
-  });
-};
-
-// 
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   document.getElementById("minmax").onclick = () => {
-//     getMinMax();
-//   }
-// });
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   document.getElementById("load_data_button").onclick = () => {
-//     // Get the selected category
-//     const category = document.getElementById("category").value;
-    
-//     // Call the function to request data based on the selected category
-//     requestDataByCategory(category);
-//   }
-// });
-
-
-
 /**
- * Assigning the callback to request the data on click.
- */
-// document.getElementById("load_data_button").onclick = () => {
-//   let max_weight = document.getElementById("max_weight").value
-//   if (!isNaN(max_weight)) {
-//     max_weight = parseFloat(max_weight)
-//   } else {
-//     max_weight = Infinity
-//   }
-//   requestData({ max_weight })
-// }
-
-/**
- * Object, that will store the loaded data.
+ * Object that will store the loaded data.
  */
 let data = {
-  pcp : undefined,
-}
-
-/**
- * Callback that is called, when the requested data was sent from the server and is received in the frontend (here).
- * @param {*} payload
- */
-let handleData = (payload) => {
-  console.log(`Fresh data from Webserver:`)
-  console.log(payload)
-  // Parse the data into the needed format for the d3 visualizations (if necessary)
-  // Here, the barchart shows two bars
-  // So the data is preprocessed accordingly
-
-  
-
-socket.on("freshData", handleData)
-}
-// let width = 0
-// let height = 0
-
-// /**
-//  * This is an example for visualizations, that are not automatically scalled with the viewBox attribute.
-//  *
-//  * IMPORTANT:
-//  * The called function to draw the data must not do any data preprocessing!
-//  * To much computational load will result in stuttering and reduced responsiveness!
-//  */
-// let checkSize = setInterval(() => {
-//   let container = d3.select(".visualizations")
-//   let newWidth = parseInt(container.style("width"))
-//   let newHeight = parseInt(container.style("height"))
-//   if (newWidth !== width || newHeight !== height) {
-//     width = newWidth
-//     height = newHeight
-//     if (data.scatterplot) draw_scatterplot(data.scatterplot)
-//   }
-// }, 100)
+  pcp: undefined,
+};
--- Code/Template/src/_public/newpcp.js
+++ Code/Template/src/_public/newpcp.js
@@ -1,31 +1,87 @@
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
-export function drawPCP(data) {
+
+
+function drawPcp(data) {
+
     const margin = {
         top: 50,
         bottom: 50,
         left: 50,
-        right: 50,
-    };
-    let width = parseInt(svg.style("width"));
-    let height = parseInt(svg.style("height"));
+        right: 50
+      };
+      const width = 1000;
+      const height = 400;
+      
+      const attributes = [
+        { name: "year", range: [1995, 2021] },
+        { name: "minplayers", range: [1, 3] },
+        { name: "maxplayers", range: [2, 8] },
+        { name: "minplaytime", range: [5, 240] },
+        { name: "maxplaytime", range: [20, 1000] }, 
+        { name: "minage", range: [8, 17] },
+        { name: "rating", range: [7, 9] },
+        { name: "num_of_reviews", range: [5157, 96512] }
+      ];
+  // Unnest the nested data
+  data = data.map(game => ({
+    ...game,
+    rating: game.rating.rating,
+    num_of_reviews: game.rating.num_of_reviews
+  }));
 
+  const xScale = d3.scalePoint()
+    .domain(attributes.map(d => d.name))
+    .range([margin.left, width - margin.right]);
 
-    svg.attr("viewBox", `0 0 ${width} ${height}`);
-    const attributes = ["Year", "Min Players", "Max Players", "Min Playtime", "Max Playtime", "Min Age"];
-    x = d3.scalePoint(attributes, [margin.left, width - margin.right]);
+  const yScales = {};
+  attributes.forEach(d => {
+    yScales[d.name] = d3.scaleLinear()
+      .domain(d.range)
+      .range([height - margin.bottom, margin.top]); // Corrected y range
+  });
 
-y ={
-    "Year": d3.scaleLinear().domain([1995, 2021]).range([margin.top, height - margin.bottom]),
-    "Min Players": d3.scaleLinear().domain([1, 3]).range([margin.top, height - margin.bottom]),
-    "Max Players": d3.scaleLinear().domain([2, 8]).range([margin.top, height - margin.bottom]),
-    "Min Playtime": d3.scaleLinear().domain([5, 240]).range([margin.top, height - margin.bottom]),
-    "Max Playtime": d3.scaleLinear().domain([20, 1000]).range([margin.top, height - margin.bottom]),
-    "Min Age": d3.scaleLinear().domain([5, 18]).range([margin.top, height - margin.bottom])
-};
+  const lineGenerator = d3.line();
+  const linePath = function (d) {
+    const points = attributes.map(attr => [xScale(attr.name), yScales[attr.name](d[attr.name])]);
+    return lineGenerator(points);
+  };
 
+  // Clear any existing SVG
+  d3.select(".pcp").selectAll("*").remove();
 
+  const pcp = d3.select(".pcp")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
+  pcp.append("g")
+    .selectAll("path")
+    .data(data)
+    .enter()
+    .append("path")
+    .attr("d", d => linePath(d))
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1);
+
+  const featureAxisG = pcp.selectAll('g.feature')
+    .data(attributes)
+    .enter()
+    .append('g')
+    .attr('class', 'feature')
+    .attr('transform', d => `translate(${xScale(d.name)},0)`);
+
+  featureAxisG.append('g')
+    .each(function (d) {
+      d3.select(this).call(d3.axisLeft(yScales[d.name]));
+    });
+
+  featureAxisG.append('text')
+    .attr('y', height - margin.bottom)
+    .attr('dy', '1em')
+    .style('text-anchor', 'middle')
+    .text(d => d.name);
 }
 
-}
+export { drawPcp };
