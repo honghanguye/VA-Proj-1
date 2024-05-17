05d5e9358e41f0f9b5fe78d1ebced56f7cb7222e
honghanguye <92738940+honghanguye@users.noreply.github.com>
2024-05-17 18:22:31 +0200

    add mouse interaction

--- Code/Template/src/_public/newpcp.js
+++ Code/Template/src/_public/newpcp.js
@@ -1,87 +1,130 @@
 import * as d3 from "d3";
 
-
-
 function drawPcp(data) {
-
     const margin = {
         top: 50,
         bottom: 50,
         left: 50,
         right: 50
-      };
-      const width = 1000;
-      const height = 400;
-      
-      const attributes = [
+    };
+    const width = 1050;
+    const height = 500;
+
+    const attributes = [
         { name: "year", range: [1995, 2021] },
         { name: "minplayers", range: [1, 3] },
         { name: "maxplayers", range: [2, 8] },
         { name: "minplaytime", range: [5, 240] },
-        { name: "maxplaytime", range: [20, 1000] }, 
+        { name: "maxplaytime", range: [20, 1000] },
         { name: "minage", range: [8, 17] },
-        { name: "rating", range: [7, 9] },
+        { name: "rating", range: [7.5, 8.8] },
         { name: "num_of_reviews", range: [5157, 96512] }
-      ];
-  // Unnest the nested data
-  data = data.map(game => ({
-    ...game,
-    rating: game.rating.rating,
-    num_of_reviews: game.rating.num_of_reviews
-  }));
+    ];
 
-  const xScale = d3.scalePoint()
-    .domain(attributes.map(d => d.name))
-    .range([margin.left, width - margin.right]);
+    // Unnest the nested data
+    data = data.map(game => ({
+        ...game,
+        rating: game.rating.rating,
+        num_of_reviews: game.rating.num_of_reviews
+    }));
 
-  const yScales = {};
-  attributes.forEach(d => {
-    yScales[d.name] = d3.scaleLinear()
-      .domain(d.range)
-      .range([height - margin.bottom, margin.top]); // Corrected y range
-  });
+    const xScale = d3.scalePoint()
+        .domain(attributes.map(d => d.name))
+        .range([margin.left, width - margin.right]);
 
-  const lineGenerator = d3.line();
-  const linePath = function (d) {
-    const points = attributes.map(attr => [xScale(attr.name), yScales[attr.name](d[attr.name])]);
-    return lineGenerator(points);
-  };
-
-  // Clear any existing SVG
-  d3.select(".pcp").selectAll("*").remove();
-
-  const pcp = d3.select(".pcp")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-  pcp.append("g")
-    .selectAll("path")
-    .data(data)
-    .enter()
-    .append("path")
-    .attr("d", d => linePath(d))
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1);
-
-  const featureAxisG = pcp.selectAll('g.feature')
-    .data(attributes)
-    .enter()
-    .append('g')
-    .attr('class', 'feature')
-    .attr('transform', d => `translate(${xScale(d.name)},0)`);
-
-  featureAxisG.append('g')
-    .each(function (d) {
-      d3.select(this).call(d3.axisLeft(yScales[d.name]));
+    const yScales = {};
+    attributes.forEach(d => {
+        yScales[d.name] = d3.scaleLinear()
+            .domain(d.range)
+            .range([height - margin.bottom, margin.top]); // Corrected y range
     });
 
-  featureAxisG.append('text')
-    .attr('y', height - margin.bottom)
-    .attr('dy', '1em')
-    .style('text-anchor', 'middle')
-    .text(d => d.name);
+    const lineGenerator = d3.line();
+    const linePath = function(d) {
+        const points = attributes.map(attr => [xScale(attr.name), yScales[attr.name](d[attr.name])]);
+        return lineGenerator(points);
+    };
+
+    const quadtree = d3.quadtree()
+        .x(d => xScale(d.name))
+        .y(d => yScales[d.name](d[d.name]))
+        .addAll(data);
+
+    // Clear any existing SVG
+    d3.select(".pcp").selectAll("*").remove();
+
+    const pcp = d3.select(".pcp")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+
+        const gameNameDiv = d3.select(".pcp").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px");
+
+        function handleMouseOver(d) {
+            d3.select(this).attr("stroke", "orange");
+            // const [x, y] = d3.pointer(event, pcp.node()); // Get mouse position relative to the SVG container
+            // const nearest = quadtree.find(x, y); // Find the nearest data point
+            
+                gameNameDiv
+                d3.select(this)
+                .style("opacity", 1)
+                .html(d.title)
+                ;
+            
+        }
+        
+
+        
+        
+    function handleMouseOut(d) {
+        d3.select(this).attr("stroke", "steelblue");
+        gameNameDiv.transition()
+            .duration(500)
+            .style("opacity", 0);
+    }
+
+
+    const lineGroup = pcp.append("g")
+        .selectAll("path")
+        .data(data)
+        .enter()
+        .append("path")
+        .attr("d", d => linePath(d))
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut);
+
+    const featureAxisG = pcp.selectAll('g.feature')
+        .data(attributes)
+        .enter()
+        .append('g')
+        .attr('class', 'feature')
+        .attr('transform', d => `translate(${xScale(d.name)},0)`);
+
+    featureAxisG.append('g')
+        .each(function(d) {
+            d3.select(this).call(d3.axisLeft(yScales[d.name]));
+        });
+
+    featureAxisG.append('text')
+        .attr('y', height - margin.bottom)
+        .attr('dy', '1em')
+        .style('text-anchor', 'middle')
+        .text(d => d.name);
+
+
+       
 }
+
+   
 
 export { drawPcp };
